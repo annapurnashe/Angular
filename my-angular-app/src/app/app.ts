d21af0739bdc4265bfeeadb2d5cdc542d9b5ea6f
annapurnashe <shembuleannapurna598gmail.com>
2025-07-15 18:11:05 +0530

    fourth commit

--- my-angular-app/src/app/app.ts
+++ my-angular-app/src/app/app.ts
@@ -1,15 +1,17 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Movie } from './movie/movie';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Movie],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('my-first-app');
+  protected readonly title = signal('my-angular-app');
 }
+
 
 
 
--- my-angular-app/src/app/movie/movie.ts
+++ my-angular-app/src/app/movie/movie.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-movie',
-  imports: [] ,
+  imports: [],
+  templateUrl: './movie.html',
   styleUrl: './movie.css'
 })
 export class Movie {
